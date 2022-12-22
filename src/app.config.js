c660a11e2d2f9bab96ad6a7975b8cbf86fcae075
South Drifted <shiy2008@gmail.com>
2022-12-22 08:54:00 +0800

    [optimize] upgrade to Taro 3.5 & webpack 5.x (#4)

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,13 @@
-export default {
-  pages: [
-    'pages/index/index'
-  ],
+module.exports = {
+  usingComponents: {
+    'van-button': '@/vant/button/index',
+    'van-icon': '@/vant/icon/index'
+  },
+  pages: ['pages/index/index'],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
   }
-}
+};
