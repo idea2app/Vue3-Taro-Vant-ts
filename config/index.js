7f413bdfadb2807b69f5248670b18c42ff7aa2a8
TechQuery <shiy2008@gmail.com>
2025-03-22 18:14:17 +0800

    [migrate] upgrade to Taro 4, Node.js 22, PNPM 9, ESLint 9 & other latest Upstream packages/actions [add] VS Code configuration [optimize] upload Mini-app artifact to GitHub release

--- config/index.js
+++ config/index.js
@@ -83,5 +83,6 @@ module.exports = function (merge) {
   if (process.env.NODE_ENV === 'development') {
     return merge({}, config, require('./dev'));
   }
+
   return merge({}, config, require('./prod'));
 };
